277bf1052637336ff45ccb6a7abf883c4add9c82
CarlosGenis <carlosgenis06@gmail.com>
2018-04-19 12:25:36 -0400

    initial

--- crystaltest/app/assets/javascripts/application.js
+++ crystaltest/app/assets/javascripts/application.js
@@ -11,5 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require foundation
 //= require turbolinks
 //= require_tree .
+//= require angular
+$(function(){ $(document).foundation(); });
